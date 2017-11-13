0ff1efe0eb8b6b94ce63d24377dcba2f22dcdc96
Unknown <lucaimorelli00.lm@gmail.com>
2017-11-13 21:00:38 +0100

    Added loghi script to every page

--- loghi.js
+++ loghi.js
@@ -1,7 +1,6 @@
-function creaLoghi()
-	{
-		var testo = "<img src=\"./img/LogoIren.png\" alt=\"Logo Iren\" class=\"Logo\">\n";
-		testo += "<img src=\"./img/LogoEcofan.png\" alt=\"Logo Ecofan\" class=\"Logo\" style=\"width:6.3%;\">\n";
-		testo +=  "<img src=\"./img/LogoAmga.png\" alt=\"Logo Amga\" class=\"Logo\">";
+function creaLoghi(){
+		var testo = "<img src=\"./img/loghi/LogoIren.png\" alt=\"Logo Iren\" class=\"Logo\">\n";
+		testo += "<img src=\"./img/loghi/LogoEcofan.png\" alt=\"Logo Ecofan\" class=\"Logo\" style=\"width:6.3%;\">\n";
+		testo +=  "<img src=\"./img/loghi/LogoAmga.png\" alt=\"Logo Amga\" class=\"Logo\">";
 		return testo;
 	}
