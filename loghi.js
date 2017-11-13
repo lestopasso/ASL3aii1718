3931f8b7a046037958ede7652d65f4caa3c6f1a4
Unknown <lucaimorelli00.lm@gmail.com>
2017-11-13 20:08:32 +0100

    Added loghi.js

---
+++ loghi.js
@@ -0,0 +1,7 @@
+function creaLoghi()
+	{
+		var testo = "<img src=\"./img/LogoIren.png\" alt=\"Logo Iren\" class=\"Logo\">\n";
+		testo += "<img src=\"./img/LogoEcofan.png\" alt=\"Logo Ecofan\" class=\"Logo\" style=\"width:6.3%;\">\n";
+		testo +=  "<img src=\"./img/LogoAmga.png\" alt=\"Logo Amga\" class=\"Logo\">";
+		return testo;
+	}
