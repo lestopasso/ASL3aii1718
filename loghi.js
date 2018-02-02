d718ae7bd4061bb9d0ae1942953184c18128a6ab
Luca Morelli <lucaimorelli00.lm@gmail.com>
2018-02-02 09:55:54 +0100

    Modifiche per la rimozione dei comandi su pagina

--- loghi.js
+++ loghi.js
@@ -1,6 +1,11 @@
+document.oncontextmenu = function() {
+    return false;
+}
+
 function creaLoghi(){
 		var testo = "<img src=\"./img/loghi/LogoIren.png\" alt=\"Logo Iren\" class=\"Logo\">\n";
 		testo += "<img src=\"./img/loghi/LogoEcofan.png\" alt=\"Logo Ecofan\" class=\"Logo\" style=\"width:6.3%;\">\n";
 		testo +=  "<img src=\"./img/loghi/LogoAmga.png\" alt=\"Logo Amga\" class=\"Logo\">";
 		return testo;
 	}
+
