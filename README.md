f87b9c2b6ca714efce1ed551544a5dfb22c353ad
Luigi Timossi <lucaimorelli00.lm@gmail.com@github.com>
2019-07-08 10:46:10 +0200

    Aggiunto tutto nuovo risolte molte cose

---
+++ README.md
@@ -0,0 +1,8 @@
+# ASL4aii1718
+Attività ASL con Iren della 4aii 2017/18
+
+Gruppo iniziale:
+Amenta, Morelli, Riggi, Siri, Stana, Suman 
+
+Gruppo estivo: 
+La Licata, Siri, Timossi, Vitali
---
+++ loghi.js
@@ -0,0 +1,12 @@
+document.oncontextmenu = function() {
+    return false;
+}
+
+function creaLoghi(){
+		var testo = "<img src=\"./img/loghi/LogoIren.png\" alt=\"Logo Iren\" class=\"Logo\">\n";
+		testo += "<img src=\"./img/loghi/LogoEcofan.png\" alt=\"Logo Ecofan\" class=\"Logo\" style=\"width:6.3%;\">\n";
+		testo +=  "<img src=\"./img/loghi/LogoAmga.png\" alt=\"Logo Amga\" class=\"Logo\">";
+		testo += "<p class="+"copyrightTesto"+">Copyright &copy; Iren S.p.A. | Tutti i diritti riservati</p>"
+		return testo;
+	}
+
